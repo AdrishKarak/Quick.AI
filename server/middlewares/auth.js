eba83462404051455b4789810a285e0f04fa6e57
Adrish <runakarak@gmail.com>
2025-10-18 10:18:09 +0530

    backend

---
+++ server/middlewares/auth.js
@@ -0,0 +1,27 @@
+import {clerkClient} from "@clerk/express";
+//check if the user is logged in and has a valid plan
+
+
+
+export const auth = async (req, res, next) => {
+  try {
+      const {userId , has} = await req.auth();
+      const hasPremiumPlan = await has({plan: 'premium_pro'})
+
+      const user = await clerkClient.users.getUser(userId);
+      if(!hasPremiumPlan && user.privateMetadata.free_usage){
+          req.free_usage = user.privateMetadata.free_usage
+      } else{
+          await clerkClient.users.updateUserMetadata(userId, {
+              privateMetadata: {
+                  free_usage: 0,
+              }
+          })
+          req.free_usage = 0;
+      }
+      req.plan = hasPremiumPlan ? 'premium_pro' : 'free';
+      next()
+  } catch (error) {
+    res.json({success: false, message: error.message})
+  }
+};
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import { clerkMiddleware , requireAuth } from '@clerk/express'
+import aiRouter from "./routes/aiRouter.js";
 
 
 const app=express();
@@ -14,6 +15,8 @@ app.use(clerkMiddleware())
 app.get('/',(req,res)=>res.send('Hello server !!'))
 
 app.use(requireAuth())
+
+app.use('/api/ai', aiRouter)
 
 const PORT = process.env.PORT || 3000;
 
