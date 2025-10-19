6a08a4f74ae30fcb9429f35bb983af31d80e9d8b
Adrish <runakarak@gmail.com>
2025-10-19 22:21:51 +0530

    backend

--- server/routes/aiRouter.js
+++ server/routes/aiRouter.js
@@ -1,9 +1,22 @@
 import express from 'express';
 import {auth} from '../middlewares/auth.js';
-import {generateArticle} from "../controllers/aiController.js";
+import {
+    generateArticle,
+    generateBlogTitle,
+    generateImage,
+    removeImageBackground,
+    removeImageObject,
+    resumeReview
+} from "../controllers/aiController.js";
+import {upload} from "../config/multer.js";
 
 const aiRouter=express.Router();
 
-aiRouter.post('generate-article' , auth , generateArticle)
+aiRouter.post('/generate-article' , auth , generateArticle)
+aiRouter.post('/generate-blog-title' , auth , generateBlogTitle)
+aiRouter.post('/generate-image' , auth , generateImage)
+aiRouter.post('/remove-image-background' ,upload.single('image') , auth , removeImageBackground)
+aiRouter.post('/remove-image-background' ,upload.single('image') , auth , removeImageObject)
+aiRouter.post('/resume-review' , auth ,resumeReview )
 
 export default aiRouter;
